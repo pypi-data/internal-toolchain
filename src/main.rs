05778c0246993841c8de136ae906ba927a0be8e5
Tom Forbes <tom@tomforb.es>
2023-07-07 00:23:25 +0100

    Switch status

--- src/main.rs
+++ src/main.rs
@@ -171,36 +171,6 @@ fn main() -> anyhow::Result<()> {
             sample,
         } => {
             github::status::get_status(&github_token, sample, progress_less_than)?;
-            let all_repos = github::projects::get_all_pypi_data_repos(&github_token)?;
-            let client = github::get_client();
-            let indexes: Result<Vec<(_, _)>, _> = all_repos
-                .iter()
-                .map(|name| {
-                    github::index::get_repository_index(&github_token, name, Some(client.clone()))
-                        .map(|r| (name, r))
-                })
-                .collect();
-            let mut indexes = indexes?;
-            // let runs: Result<Vec<_>, _> = all_repos
-            //     .iter()
-            //     .map(|name| {
-            //         github::workflows::get_workflow_runs(&github_token, name, Some(client.clone()))
-            //     })
-            //     .collect();
-            // let runs = runs?;
-
-            if let Some(sample) = sample {
-                let mut rng = thread_rng();
-                indexes = indexes.into_iter().choose_multiple(&mut rng, sample);
-            }
-
-            for (name, index) in indexes {
-                let stats = index.stats();
-                if stats.percent_done() < progress_less_than {
-                    println!("{name}")
-                }
-                eprintln!("Stats: {stats:?}: percent done: {}%", stats.percent_done());
-            }
         }
         Commands::TriggerCi {
             name,
