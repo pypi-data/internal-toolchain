9f4ad8eae0a3ea4cb1091d7486fea57cb0c3c076
Tom Forbes <tom@tomforb.es>
2023-07-08 14:56:19 +0100

    Fix status memory usage

--- src/github/status.rs
+++ src/github/status.rs
@@ -8,8 +8,7 @@ use polars::prelude::*;
 
 use rayon::prelude::*;
 use serde::Serialize;
-use std::fs::File;
-use std::io::BufWriter;
+
 use ureq::Agent;
 use url::Url;
 
@@ -47,7 +46,7 @@ impl RepoStatus {
         .unwrap()
     }
     #[cfg(not(feature = "stats"))]
-    pub fn get_detailed_stats(&self, client: Agent) -> DetailedStats {
+    pub fn get_detailed_stats(&self, _client: Agent) -> DetailedStats {
         panic!("stats feature not enabled");
     }
 
@@ -157,46 +156,36 @@ impl RepoStatus {
 pub fn get_status(github_token: &str, with_runs: bool) -> Result<Vec<RepoStatus>, GithubError> {
     let all_repos = github::projects::get_all_pypi_data_repos(github_token)?;
     let client = github::get_client();
-    let indexes: Result<Vec<(_, _)>, _> = all_repos
+    let indexes: Result<Vec<RepoStatus>, GithubError> = all_repos
         .into_par_iter()
         .map(|repo| {
-            github::index::get_repository_index(github_token, &repo.name, Some(client.clone()))
-                .map(|r| (repo, r))
-        })
-        .collect();
-    let indexes = indexes?
-        .into_par_iter()
-        .map(|(repo, index)| {
+            let index = github::index::get_repository_index(
+                github_token,
+                &repo.name,
+                Some(client.clone()),
+            )?;
             let workflow_runs = if with_runs {
                 let runs = github::workflows::get_workflow_runs(
                     github_token,
                     &repo.name,
                     Some(client.clone()),
                     5,
-                )
-                .unwrap();
+                )?;
                 Some(runs.workflow_runs)
             } else {
                 None
             };
             let stats = index.stats();
-
-            RepoStatus {
+            let status = RepoStatus {
                 name: repo.name,
                 percent_done: stats.percent_done(),
                 stats,
                 workflow_runs,
                 size_kb: repo.size as u64,
-            }
+            };
+            Ok(status)
         })
         .collect();
 
-    Ok(indexes)
-    // let runs: Result<Vec<_>, _> = all_repos
-    //     .iter()
-    //     .map(|name| {
-    //         github::workflows::get_workflow_runs(&github_token, name, Some(client.clone()))
-    //     })
-    //     .collect();
-    // let runs = runs?;
+    indexes
 }
